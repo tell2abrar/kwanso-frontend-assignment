0ace0bff6ea7af1e602d07c820bb20e49622f7c0
Abrar Azam <tell2abrar@gmail.com>
2023-01-25 20:32:11 +0500

    redirect to list-tasks when land on home

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,23 @@ import router from 'navigation/router';
 import AppContext from 'AppContext';
 import primaryTheme from 'theme/primaryTheme';
 import { Task } from 'types/task';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { RouterSwitch } from 'react-typesafe-routes';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 
 const App = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
 
+  useEffect(() => {}, []);
+
   return (
     <div>
       <AppContext.Provider value={{ tasks, setTasks }}>
         <BrowserRouter>
+          <Route exact path='/'>
+            {<Redirect to='/list-tasks' />}
+          </Route>
           <ChakraProvider theme={primaryTheme}>
             <Layout>
               <RouterSwitch router={router} />
